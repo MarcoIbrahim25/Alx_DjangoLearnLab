b232c50624820f91e82193d244a4e06a165105d3
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-09-12 07:24:19 +0300

    New

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),  # Function-based view
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    pk_url_kwarg = 'pk'
+
+    def get_queryset(self):
+        return (Library.objects
+                .prefetch_related('books__author'))  
