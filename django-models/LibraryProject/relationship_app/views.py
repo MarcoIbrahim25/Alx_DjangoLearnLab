24adda808dc2c38b5fbcd54436351f321432adc3
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-09-12 07:27:49 +0300

    New

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,9 @@ from django.views.generic import DetailView
 from .models import Book, Library
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+   def list_books(request):
+    books = Book.objects.all()   
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
