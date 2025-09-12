8b046b4eba6e85d990a0d269252f96d3b1f59f21
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-09-12 07:32:32 +0300

    New

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,16 +3,10 @@ from django.views.generic import DetailView
 from .models import Book, Library
 
 def list_books(request):
-   def list_books(request):
-    books = Book.objects.all()   
+    books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
-    pk_url_kwarg = 'pk'
-
-    def get_queryset(self):
-        return (Library.objects
-                .prefetch_related('books__author'))  
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
