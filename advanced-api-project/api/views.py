8411e5131b262828210e8ce182d8f6129a6c5d79
MarcoSerry <marcoserry88@gmail.com>
2025-09-30 20:46:16 +0300

    New

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,21 +2,27 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateView(generics.ListCreateAPIView):
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def get_queryset(self):
-        qs = Book.objects.all().order_by('-publication_year')
-        title = self.request.query_params.get('title')
-        year = self.request.query_params.get('year')
-        if title:
-            qs = qs.filter(title__icontains=title)
-        if year:
-            qs = qs.filter(publication_year=year)
-        return qs
-
-class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
