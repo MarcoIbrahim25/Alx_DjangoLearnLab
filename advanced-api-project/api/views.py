c9d6e521a413228350b6bd57acbcb20ca0fed860
MarcoSerry <marcoserry88@gmail.com>
2025-09-30 20:44:25 +0300

    New

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListCreateView(generics.ListCreateAPIView):
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        qs = Book.objects.all().order_by('-publication_year')
+        title = self.request.query_params.get('title')
+        year = self.request.query_params.get('year')
+        if title:
+            qs = qs.filter(title__icontains=title)
+        if year:
+            qs = qs.filter(publication_year=year)
+        return qs
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
