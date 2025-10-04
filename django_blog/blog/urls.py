5d6b173592bccccb8bbda55681acf7343c444f66
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-10-04 10:43:37 +0300

    New

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("posts/", views.post_list, name="posts"),
+    path("posts/", views.PostListView.as_view(), name="posts"),
+    path("posts/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-edit"),
+    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
     path("login/", LoginView.as_view(template_name="auth/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="auth/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, UserUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm, UserUpdateForm, PostForm
+from .models import Post
 
 def home(request):
     return render(request, "home.html")
 
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "posts/post_confirm_delete.html"
+    success_url = reverse_lazy("posts")
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
 def post_list(request):
-    return render(request, "posts.html")
+    return redirect("posts")
 
 def login_view_placeholder(request):
     return render(request, "login.html")
