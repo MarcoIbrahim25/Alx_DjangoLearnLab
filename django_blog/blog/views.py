94b8f42fd48c1ed3ffbc60e422edc4e237ff20d3
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-10-04 11:02:38 +0300

    New

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,19 +12,19 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = "posts/post_list.html"
+    template_name = "blog/posts/listing.html"
     context_object_name = "posts"
     ordering = ["-published_date"]
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = "posts/post_detail.html"
+    template_name = "blog/posts/viewing.html"
     context_object_name = "post"
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = "posts/post_form.html"
+    template_name = "blog/posts/creating.html"
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -32,18 +32,19 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = "posts/post_form.html"
+    template_name = "blog/posts/editing.html"
     def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = "posts/post_confirm_delete.html"
+    template_name = "blog/posts/deleting.html"
     success_url = reverse_lazy("posts")
     def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
+
 
 def post_list(request):
     return redirect("posts")
