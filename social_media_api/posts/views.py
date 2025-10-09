212270a547a73caad8bd82f757b1db735ba1220c
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-10-09 08:35:45 +0300

    New

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """يُسمح فقط للمالك بالتعديل أو الحذف."""
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class StandardPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 20
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = StandardPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = StandardPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
