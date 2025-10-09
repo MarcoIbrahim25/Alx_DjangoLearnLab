fd39ef37f4f1c0354e6840f0733061633cc30de6
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-10-09 08:28:21 +0300

    New

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,29 +1,27 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from .models import User
+from rest_framework.authtoken.models import Token
 
-class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
-    following_count = serializers.IntegerField(source='following.count', read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture',
-                  'followers_count', 'following_count']
-        read_only_fields = ['id', 'followers_count', 'following_count']
-
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        # إنشاء المستخدم
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+        )
+        # إنشاء التوكن
+        Token.objects.create(user=user)
         return user
 
 
@@ -34,6 +32,12 @@ class LoginSerializer(serializers.Serializer):
     def validate(self, data):
         user = authenticate(username=data.get('username'), password=data.get('password'))
         if not user:
-            raise serializers.ValidationError('Invalid credentials')
+            raise serializers.ValidationError('Invalid username or password')
         data['user'] = user
         return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
